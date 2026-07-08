833777806b8c7532b88e66886e77597b0b7bbf55
Avika <avikavg@gmail.com>
2026-07-08 13:54:38 -0700

    position 2

---
+++ position2.py
@@ -0,0 +1,39 @@
+f = open("/share/SARS-class/SARS-2020.fasta")
+lines = f.readlines()
+for i in range(len(lines)):
+   lines[i] = lines[i].rstrip('\n')
+
+string = ""
+
+for i in range(len(lines)):
+   string += lines[i]
+gencode = {
+    'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+    'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+    'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+    'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+    'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+    'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+    'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+    'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+    'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+    'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+    'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+    'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+    'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+    'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+    'TAC':'Y', 'TAT':'Y', 'TAA':'*', 'TAG':'*',
+    'TGC':'C', 'TGT':'C', 'TGA':'*', 'TGG':'W'}
+
+codon = ""
+protein = ""
+
+for i in range (2, len(string), 3):
+   codon = string [i: i+3]
+   if codon in gencode:
+      protein += (gencode[codon])
+   else:
+      print("")
+
+print(protein)
+
