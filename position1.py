c0ce7876710f9b9bb94a07c50793869248bea182
anahataG <anahataguha@gmail.com>
2026-07-08 13:31:39 -0700

    updated position 1 - found proteins

--- position1.py
+++ position1.py
@@ -1,5 +1,3 @@
-import re
-
 f=open("/share/SARS-class/SARS-2020.fasta")
 
 lines = f.readlines()
@@ -10,9 +8,31 @@ string = ""
 
 for i in range(len(lines)):
    string += lines[i]
+gencode = {
+    'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+    'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+    'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+    'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+    'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+    'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+    'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+    'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+    'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+    'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+    'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+    'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+    'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+    'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+    'TAC':'Y', 'TAT':'Y', 'TAA':'*', 'TAG':'*',
+    'TGC':'C', 'TGT':'C', 'TGA':'*', 'TGG':'W'}
 
 codon = ""
+protein = ""
 
 for i in range (0, len(string), 3):
    codon = string [i: i+3]
-   print (codon)
+   if codon in gencode:
+      protein += (gencode[codon])
+   else:
+      print("")
+print(protein)
